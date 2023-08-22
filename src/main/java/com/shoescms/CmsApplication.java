9edc0e268a17fbfe28ba82bbd4880d31b8c4fdcc
Kai <Haunv1503@gmail.com>
2023-08-23 01:31:37 +0700

    update

--- src/main/java/com/shoescms/CmsApplication.java
+++ src/main/java/com/shoescms/CmsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CmsApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("cusTomEnv.hello_work", "124");
 		SpringApplication.run(CmsApplication.class, args);
 	}
 
